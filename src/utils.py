80e036a0151dfc429a653991b2221d2c5a681bba
0x7FFFFF <w13657804853@outlook.com>
2020-12-28 18:17:32 +0800

    修复转账数值可为负数的bug

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ from .uconomy import Shop, Uconomy
 from .user import User
 from decimal import Decimal
 from config.bot_config import regiser_reward,regiser_command,search_item_command
-from .exception import IllegalSteamIdError,BalanceNotEnoughError,IllggalCommandFormatError
+from .exception import IllegalSteamIdError,BalanceNotEnoughError,IllggalCommandFormatError,ValueIsNegativeError
 from graia.application.message.elements.internal import At,Plain
 from graia.application.message.chain import MessageChain
 
@@ -26,11 +26,14 @@ def transfer(msgchain: MessageChain,qid: str):
         other_side_steamid = user.getSteamId(msgchain.get(At)[0].target)
         main_side_steamid = user.getSteamId(qid)
         transfer_balance = Decimal(int(msgchain.get(Plain)[1].text))
-        if uconomy.getUserBalance(main_side_steamid) >= transfer_balance:
-            uconomy.setUserBalance(main_side_steamid,uconomy.getUserBalance(main_side_steamid)-transfer_balance)
-            uconomy.setUserBalance(other_side_steamid,uconomy.getUserBalance(other_side_steamid)+transfer_balance)
+        if transfer_balance <= Decimal(0):
+            raise ValueIsNegativeError
         else:
-            raise BalanceNotEnoughError
+            if uconomy.getUserBalance(main_side_steamid) >= transfer_balance:
+                uconomy.setUserBalance(main_side_steamid,uconomy.getUserBalance(main_side_steamid)-transfer_balance)
+                uconomy.setUserBalance(other_side_steamid,uconomy.getUserBalance(other_side_steamid)+transfer_balance)
+            else:
+                raise BalanceNotEnoughError
     except IndexError:
         raise IllggalCommandFormatError
 
